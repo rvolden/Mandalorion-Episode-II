83a58b510e80640ce6db24d666d36698572a4bc4
Roger Volden <rvolden@ucsc.edu>
2018-03-22 16:37:49 -0700

    Style changes

--- Mandalorion_12_Create_Consensi_New.py
+++ Mandalorion_12_Create_Consensi_New.py
@@ -6,20 +6,18 @@ import sys
 import os
 import numpy as np
 
-path=sys.argv[1]
-temp_folder=path+'/parsed_reads'
-subsample=int(sys.argv[2])
+path = sys.argv[1]
+temp_folder = path + '/parsed_reads'
+subsample = int(sys.argv[2])
 #os.system('mkdir '+temp_folder)
-
 
 poa = 'poa'
 score_matrix = '/home/vollmers/scripts/ONT/NUC.4.4.mat'
   # Change this if you want to subsample more or less reads per isoform
 
-racon='/home/vollmers/Downloads/racon/bin/racon'
-minimap2='/home/vollmers/Downloads/minimap2-2.5_x64-linux/minimap2'
-consensus='/home/vollmers/scripts/ONT/R2C2/consensus.py'
-
+racon = '/home/vollmers/Downloads/racon/bin/racon'
+minimap2 = '/home/vollmers/Downloads/minimap2-2.5_x64-linux/minimap2'
+consensus = '/home/vollmers/scripts/ONT/R2C2/consensus.py'
 
 def read_fastq_file(seq_file):
     '''
@@ -51,60 +49,50 @@ def read_fastq_file(seq_file):
         average_quals = np.average(quals)
         seq_length = len(seq)
         read_list.append((name, seed, seq, qual, average_quals, seq_length))
-
         lineNum += 4
     return read_list
 
-
-def read_fasta(infile):
-    reads={}
-    sequence=''
-    name=''
-    for line in open(infile):
-        a=line.strip()
-        if a[0]=='>':
-            if sequence!='':
-
-                reads[name]=sequence
-            name=a[1:].split()[0]
-            sequence=''
+def read_fasta(inFile):
+    '''Reads in FASTA files, returns a dict of header:sequence'''
+    readDict = {}
+    tempSeqs, headers, sequences = [], [], []
+    for line in open(inFile):
+        line = line.rstrip()
+        if not line:
+            continue
+        if line.startswith('>'):
+            headers.append(line.split()[0][1:])
+        # covers the case where the file ends while reading sequences
+        if line.startswith('>'):
+            sequences.append(''.join(tempSeqs).upper())
+            tempSeqs = []
         else:
-            sequence+=a
-    reads[name]=sequence
-    return reads
+            tempSeqs.append(line)
+    sequences.append(''.join(tempSeqs).upper())
+    sequences = sequences[1:]
+    for i in range(len(headers)):
+        readDict[headers[i]] = sequences[i]
+    return readDict
 
-def reverse_complement(sequence):
-  Seq=''
-  complement = {'A':'T','C':'G','G':'C','T':'A','N':'N','-':'-'}
-  for item in sequence[::-1]:
-    Seq=Seq+complement[item]
-  return Seq
-
-
-def determine_consensus(name, fasta,fastq):
+def determine_consensus(name, fasta, fastq):
     '''Aligns and returns the consensus'''
     print('determine')
     corrected_consensus = ''
-
     out_F = fasta
+    fastq_reads = read_fastq_file(fastq)
+    out_Fq = temp_folder + '/subsampled.fastq'
+    out = open(out_Fq, 'w')
 
-    fastq_reads=read_fastq_file(fastq)
-
-    out_Fq=temp_folder + '/subsampled.fastq'
-
-    out=open(out_Fq,'w')
-
-
-    indexes=np.random.choice(np.arange(0,len(fastq_reads),1),min(len(fastq_reads),subsample))
-    print('XXXXXXXXXXXXXXXXXX',indexes)
-    subsample_fastq_reads=[]
+    indexes = np.random.choice(np.arange(0, len(fastq_reads)),
+                               min(len(fastq_reads), subsample))
+    print('XXXXXXXXXXXXXXXXXX', indexes)
+    subsample_fastq_reads = []
     for index in indexes:
         subsample_fastq_reads.append(fastq_reads[index])
 
     for read in subsample_fastq_reads:
-
-        out.write('@'+read[0]+'_'+str(read[1])+'\n'+read[2]+'\n+\n'+read[3]+'\n')
-
+        out.write('@' + read[0] + '_' + str(read[1]) + '\n'
+                  + read[2] + '\n+\n' + read[3] + '\n')
     out.close()
 
     poa_cons = temp_folder + '/consensus.fasta'
@@ -112,14 +100,12 @@ def determine_consensus(name, fasta,fastq):
     overlap = temp_folder +'/overlaps.sam'
     pairwise = temp_folder + '/prelim_consensus.fasta'
 
-
 #    PIR = temp_folder + '/' + name + 'alignment.fasta'
-#    os.system('%s -read_fasta %s -hb -pir %s \
-#               %s >./poa_messages.txt 2>&1' \
+#    os.system('%s -read_fasta %s -hb -pir %s
+#              %s >./poa_messages.txt 2>&1'
 #              %(poa, out_F, PIR, score_matrix))
 #    reads = read_fasta(PIR)
 #    print('poa done')
-
 
 #    if repeats == '2':
 #        Qual_Fasta = open(pairwise, 'w')
@@ -127,52 +113,56 @@ def determine_consensus(name, fasta,fastq):
 #            if 'CONSENS' not in read:
 #                Qual_Fasta.write('>' + read + '\n' + reads[read] + '\n')
 #        Qual_Fasta.close()
-#        os.system('%s %s %s %s >> %s' \
+#        os.system('%s %s %s %s >> %s'
 #                  %(consensus, pairwise, out_Fq, name, poa_cons))
 
 #    else:
 #        for read in reads:
 #          if 'CONSENS0' in read:
 #            out_cons_file = open(poa_cons, 'w')
-#            out_cons_file.write('>' + name + '\n' \
+#            out_cons_file.write('>' + name + '\n'
 #                                + reads[read].replace('-', '') + '\n')
 #            out_cons_file.close()
 #    print('cosensus done')
 
-    max_coverage=0
-    reads=read_fasta(out_F)
-    repeats=str(len(reads))
+    max_coverage = 0
+    reads = read_fasta(out_F)
+    repeats = str(len(reads))
     for read in reads:
-        coverage=int(read.split('_')[3])
-        if coverage>max_coverage:
-             best=read
-             max_coverage=coverage
+        coverage = int(read.split('_')[3])
+        if coverage > max_coverage:
+             best = read
+             max_coverage = coverage
 
     out_cons_file = open(poa_cons, 'w')
-    out_cons_file.write('>' + best + '\n' + reads[best].replace('-', '') + '\n')
+    out_cons_file.write('>' + best + '\n'
+                        + reads[best].replace('-', '') + '\n')
     out_cons_file.close()
 
 
-    final=poa_cons
-    for i in np.arange(1,2,1):
+    final = poa_cons
+    for i in np.arange(1, 2):
         try:
-            if i==1:
-                input_cons=poa_cons
-                output_cons=poa_cons.replace('.fasta','_'+str(i)+'.fasta')
+            if i == 1:
+                input_cons = poa_cons
+                output_cons = poa_cons.replace('.fasta',
+                                               '_' + str(i) + '.fasta')
             else:
-                input_cons=poa_cons.replace('.fasta','_'+str(i-1)+'.fasta')
-                output_cons=poa_cons.replace('.fasta','_'+str(i)+'.fasta')
+                input_cons = poa_cons.replace('.fasta',
+                                              '_'+str(i-1) + '.fasta')
+                output_cons = poa_cons.replace('.fasta',
+                                               '_' + str(i) + '.fasta')
 
-            print(i,minimap2,input_cons,out_Fq,overlap)
-            os.system('%s --secondary=no -ax map-ont \
-                      %s %s > %s 2> ./minimap2_messages.txt' \
+            print(i, minimap2, input_cons, out_Fq, overlap)
+            os.system('%s --secondary=no -ax map-ont
+                      %s %s > %s 2> ./minimap2_messages.txt'
                       % (minimap2, input_cons, out_Fq, overlap))
             print('minimap2 done')
-            os.system('%s --sam --bq 5 -t 1 \
-                      %s %s %s %s >> ./racon_messages.txt 2>&1' \
+            os.system('%s --sam --bq 5 -t 1
+                      %s %s %s %s >> ./racon_messages.txt 2>&1'
                       %(racon,out_Fq, overlap, input_cons, output_cons))
             print('racon done')
-            final=output_cons
+            final = output_cons
         except:
             pass
 
@@ -180,30 +170,22 @@ def determine_consensus(name, fasta,fastq):
     reads = read_fasta(final)
 #    if len(reads)==0:
 #        reads = read_fasta(poa_cons)
-
     for read in reads:
         corrected_consensus = reads[read]
-
     return corrected_consensus, repeats
 
-
-
-combined_consensus_file=open(path+'/Isoform_Consensi.fasta','w')
+combined_consensus_file = open(path + '/Isoform_Consensi.fasta', 'w')
 combined_consensus_file.close()
 
-
-determine_consensus
-
-
-
-for line in open(path+'/isoform_list'):
-    fasta=line.split('\t')[0]
-    fastq=line.split('\t')[1]
-    name=line.split('\t')[2].strip()
+for line in open(path + '/isoform_list'):
+    fasta = line.split('\t')[0]
+    fastq = line.split('\t')[1]
+    name = line.split('\t')[2].strip()
     print(line)
-    print(fasta,fastq,name)
-    corrected_consensus,repeats=determine_consensus(name,fasta,fastq)
-    combined_consensus_file=open(path+'/Isoform_Consensi.fasta','a')
+    print(fasta, fastq, name)
+    corrected_consensus, repeats = determine_consensus(name, fasta, fastq)
+    combined_consensus_file = open(path + '/Isoform_Consensi.fasta', 'a')
 
-    combined_consensus_file.write('>'+name+'_'+repeats+'\n'+corrected_consensus+'\n')
+    combined_consensus_file.write('>' + name + '_' + repeats + '\n'
+                                  +corrected_consensus + '\n')
     combined_consensus_file.close()
